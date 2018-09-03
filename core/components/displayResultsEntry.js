2a423a09ea4e6764e968f581d46b561187c1d0d1
Ryan Blahnik <ryanblahnik@gmail.com>
2018-09-03 16:53:40 -0500

    Deployment loading

--- core/components/displayResultsEntry.js
+++ core/components/displayResultsEntry.js
@@ -6,6 +6,6 @@ angular.module('ngular')
     template:
       `
         <strong>{{$ctrl.entry.result}}</strong>
-        <em style="float:right">{{$ctrl.entry.moment}}</em><br>
+        <em style="float:right">{{$ctrl.entry.moment}} UTC</em><br>
       `
   });
